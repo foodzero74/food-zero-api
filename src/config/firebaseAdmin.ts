570f29fc1724655aa912f6c7b99958aa5991f65f
Cristian Ceballos <cmceballos88@gmail.com>
2023-09-03 20:11:06 +0200

    Change credentials from json to .env for security

--- src/config/firebaseAdmin.ts
+++ src/config/firebaseAdmin.ts
@@ -1,5 +1,22 @@
 import * as admin from 'firebase-admin';
-import * as serviceAccount from '../serviceAccountKey.json';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+};
 
 if (!admin.apps.length) {
   admin.initializeApp({
@@ -8,4 +25,3 @@ if (!admin.apps.length) {
 }
 
 export default admin;
-
